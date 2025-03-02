afe1ce4e3dbe6245435f0321da568d9c124642f7
Pragya Jhunjhunwala <pjhunjh1@uci.edu>
2025-03-01 19:18:11 -0800

    removing some files + twilio

---
+++ twilio.py
@@ -0,0 +1,22 @@
+import requests
+import json
+
+with open('secrets.json') as f:
+    secrets = json.load(f)
+
+auth_token = secrets['auth_token']
+account_sid = secrets['account_sid']
+URL = secrets['url']
+
+url = URL
+data = {
+    'To': '+18777804236',
+    'From': '+18779089736',
+    'Body': 'Hello from Twilio'
+}
+auth = (account_sid,auth_token)
+
+response = requests.post(url, data=data, auth=auth)
+
+print(response.status_code)
+print(response.json())
